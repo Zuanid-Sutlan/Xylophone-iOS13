627aebb2387ff7418fb4421bec88891a3714098e
Zunaid_Sultan <zunaidsultan786@gmail.com>
2025-02-05 12:38:34 +0500

    initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    var myPlyer: AVAudioPlayer!
+    
+    let sounds = ["A", "B", "C", "D", "E", "F", "G"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupAudioSession()
     }
-
-
     
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        var soundName = sender.currentTitle ?? sounds.randomElement()!
+        print("start")
+        sender.alpha = 0.5
+        playSound(fileName: soundName)
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        print("End")
+    }
+    
+    
+    
+    func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Audio session setup failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func playSound(fileName: String) {
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "wav") else {
+            print("Sound file \(fileName) not found.")
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
+        }
+    }
+    
+    func playMySound(fileName: String){
+        let url = Bundle.main.url(forResource: fileName, withExtension: "wav")
+        myPlyer = try! AVAudioPlayer(contentsOf: url!)
+        myPlyer.play()
+    }
+    
 }
 
